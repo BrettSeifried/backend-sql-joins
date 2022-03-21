fc7e37994a2815335de96667929ce1d17bacee69
Brett Seifried <seifriedbrett@gmail.com>
2022-03-21 12:17:00 -0700

    passing 2nd test

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,12 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+SELECT
+    *
+FROM
+    city
+INNER JOIN
+    address
+ON
+    city.city_id = address.city_id
+WHERE
+	city.city = 'Dundee';
